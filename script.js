b07b507a7aab05d991407bdbfe156f46bbe33e10
sizar <sizar@gmail.com>
2022-06-16 12:24:16 +0600

    Math Random Number Fach

--- script.js
+++ script.js
@@ -1,35 +1,60 @@
 
-	// alert('Hi,Kawsar Khan Sizar');
+	alert('Hi,Kawsar Khan Sizar');
 
-	// document.write("Hello,SIR");
+	document.write("Hello,SIR");
 
-	// console.log('just fuck you baby!');
+	console.log('just fuck you baby!');
 
-	// console.log('hi hi hi ');
+	console.log('hi hi hi ');
 
-	// var exmple = "I am a variable";
+	var exmple = "I am a variable";
 
-	// document.write(exmple);
+	document.write(exmple);
 
- // var VisitorAge = 	prompt("what is your age?");
+ var VisitorAge = 	prompt("what is your age?");
 
- // var VisitorName = prompt("what is your name?");
+ var VisitorName = prompt("what is your name?");
 
- // document.write("Hello " + VisitorName + " your current age as of today is " + VisitorAge);
+ document.write("Hello " + VisitorName + " your current age as of today is " + VisitorAge);
 
- // var name = window.prompt("Enter your name: ");
- //   alert("Your name is " + name);
+ var name = window.prompt("Enter your name: ");
+   alert("Your name is " + name);
 
-// var hotText = 'MDN';
-// var url = 'https://developer.mozilla.org/';
+var hotText = 'MDN';
+var url = 'https://developer.mozilla.org/';
 
-// document.write('Click to return to ' + hotText.link(url));
+document.write('Click to return to ' + hotText.link(url));
 
 const worldString = 'Hello, world';
 
 document.write(worldString.fontcolor('red') +  ' is red in this line');
-// '<font color="red">Hello, world</font> is red in this line'
+'<font color="red">Hello, world</font> is red in this line'
 
 document.write(worldString.fontcolor('FF00') + ' is red in hexadecimal in this line');
-// '<font color="FF00">Hello, world</font> is red in hexadecimal in this line'
+'<font color="FF00">Hello, world</font> is red in hexadecimal in this line'
 
+alert('Welcome');
+var VisitorName = prompt('What is your name?');
+var VisitorFavouriteColor = prompt('What is your favourite color?');
+document.write('Hello; ' +VisitorName+ ' your favourite color is ' +VisitorFavouriteColor);
+console.log('programm complated');
+
+var VisitorAge = prompt("How Many Years Have You Been Alive for?");
+var YearsAlive = parseInt(VisitorAge);
+YearsAlive = YearsAlive * 365 ;
+document.write("You Have Been Alive For " +YearsAlive+ " Days!!!");
+
+
+var RNumber = Math.random();
+alert(RNumber);
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+console.log(getRandomInt(7));
+console.log(Math.random());
+
+var UserNumber  = prompt('Enter your number');
+UserNumber = parseInt(UserNumber);
+var RNumber = Math.floor(Math.random() *UserNumber +1);
+alert("Your random number is " +RNumber);
