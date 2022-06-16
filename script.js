d17760a67dd37daafc76ba35ea67437b5d0307eb
sizar <sizar@gmail.com>
2022-06-16 10:33:04 +0600

    javascript learning(basic)

---
+++ script.js
@@ -0,0 +1,35 @@
+
+	// alert('Hi,Kawsar Khan Sizar');
+
+	// document.write("Hello,SIR");
+
+	// console.log('just fuck you baby!');
+
+	// console.log('hi hi hi ');
+
+	// var exmple = "I am a variable";
+
+	// document.write(exmple);
+
+ // var VisitorAge = 	prompt("what is your age?");
+
+ // var VisitorName = prompt("what is your name?");
+
+ // document.write("Hello " + VisitorName + " your current age as of today is " + VisitorAge);
+
+ // var name = window.prompt("Enter your name: ");
+ //   alert("Your name is " + name);
+
+// var hotText = 'MDN';
+// var url = 'https://developer.mozilla.org/';
+
+// document.write('Click to return to ' + hotText.link(url));
+
+const worldString = 'Hello, world';
+
+document.write(worldString.fontcolor('red') +  ' is red in this line');
+// '<font color="red">Hello, world</font> is red in this line'
+
+document.write(worldString.fontcolor('FF00') + ' is red in hexadecimal in this line');
+// '<font color="FF00">Hello, world</font> is red in hexadecimal in this line'
+
