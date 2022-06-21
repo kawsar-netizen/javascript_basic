0ab5cd2fac3eb06af09465664753750c87edc6f5
sizar <sizar@gmail.com>
2022-06-21 11:53:52 +0600

    Array function

---
+++ array.js
@@ -0,0 +1,21 @@
+var OurArray = [
+"Kawsar",
+1210,
+"Sinat",
+"sizar"
+];
+
+alert("Hi,Khan");
+
+OurArray.push("Dia","Kamal","Nafian");
+OurArray.shift(1);
+OurArray.pop();
+
+document.write("<p>" + OurArray[0] + "</p>");
+document.write("<p>" + OurArray[1] + "</p>");
+document.write("<p>" + OurArray[2] + "</p>");
+document.write("<p>" + OurArray[3] + "</p>");
+document.write("<p>" + OurArray[4] + "</p>");
+document.write("<p>" + OurArray[5] + "</p>");
+document.write("<p>" + OurArray[6] + "</p>");
+
